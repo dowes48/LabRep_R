3eea067d666d673bd24905147dd59e3171ce7957
dowes48 <dowes48@gmail.com>
2026-01-15 07:48:33 -0800

    R files

--- R_wrangle_AbLabs_13.r
+++ R_wrangle_AbLabs_13.r
@@ -6,15 +6,29 @@ iter  <- "13"
 in_dir <- "AbLab_Rpts/AbLab_2021"
 in_pattern <- "AbLabs_.+\\.prn"
 out_dir <- "Output"
-out_name <- str_c("RS_", iter, ".csv")
+out_name <- str_c("RStudio_", iter, ".csv")
+out_fullName <- file.path(out_dir, out_name)
+
+clean_lab_vals <- list(name="", tickNo="", gluc="")
+lab_vals <- clean_lab_vals
 
 get_values <- function(line) {
     if(str_detect(line,"NAME")) {
-        print(str_sub(line, start=6, end=-1))
+        lab_vals$name <<- str_trim(str_sub(line, start=6, end=-1), side="both")
         return()
     }
-    else if(str_detect(line,"TICKET NUMBER:")){
-        print(str_sub(line, start=15, end=25))
+    else if(str_detect(line,"TICKET NUMBER")){
+        lab_vals$tickNo <<- str_trim(str_sub(line, start=15, end=25), side="both")
+        return()
+    }
+    else if(str_detect(line,"GLUCOSE ")){
+        lab_vals$gluc <<- str_trim(str_sub(line, start=30, end=40), side="both")
+    }
+    else if(str_detect(line,"\f")){
+        tempStr <- str_c(lab_vals, collapse=",")
+        writeLines(tempStr, con=file_out)
+        lab_vals <<- clean_lab_vals
+        return()
     }
     else return()
 }
@@ -25,6 +39,11 @@ process_lines <- function(lines){
         }
 }
 
+#*************************************************************
+
+file_out <- file(out_fullName,"w")
+temp_Str <- "name,ticket,gluc"
+writeLines(temp_Str, con=file_out)
 file_list <- list.files(path=in_dir, pattern=in_pattern, recursive=TRUE)
 for(fname in file_list) {
     in_file <- file(file.path(in_dir, fname), "r")
@@ -32,3 +51,4 @@ for(fname in file_list) {
     close(in_file)
     process_lines(in_lines_list)
 }
+close(file_out)
